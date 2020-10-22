a042d368125cb82d71015523966c8f06e3c17b3c
EvertonHimself <evertondeoliveira@gmail.com>
2020-10-22 07:35:13 -0300

    Created the level exit.
    Created the level exit and copied some scenes.

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float loadLevelDelay = 2f;
+    [SerializeField] float levelExitSlowMoFactor = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = levelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(loadLevelDelay);
+        Time.timeScale = 1f;
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
